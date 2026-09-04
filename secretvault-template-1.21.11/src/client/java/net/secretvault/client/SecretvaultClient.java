7eb4941dd7b51b2ba004f4d64c251bb469fc3ab9
Arin Chatterjee <arinchatterg@gmail.com>
2026-09-04 18:22:45 +0530

    Add files via upload

---
+++ secretvault-template-1.21.11/src/client/java/net/secretvault/client/SecretvaultClient.java
@@ -0,0 +1,10 @@
+package net.secretvault.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SecretvaultClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
